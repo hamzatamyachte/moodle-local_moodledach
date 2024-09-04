b0fedc2852d320842c6611d7cbd07ed5daa74a93
Hamza Tamyachte <hamza.tamyachte@gmail.com>
2024-09-04 10:52:56 +0200

    Create version.php

---
+++ version.php
@@ -0,0 +1,12 @@
+
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_moodledach';
+$plugin->version = 2024090206;
+$plugin->release = '4.1.1';
+$plugin->requires = 2014051200;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->cron = 0;
+$plugin->dependencies = [];
